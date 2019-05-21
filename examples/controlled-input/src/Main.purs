65a3cd795f27d76ae7678a891d453ca00b301b61
Dustin Whitney <dustin.whitney@gmail.com>
2019-05-21 11:10:56 -0400

    first attempt at capture/merge

--- examples/controlled-input/src/Main.purs
+++ examples/controlled-input/src/Main.purs
@@ -3,10 +3,11 @@ module Main where
 import Prelude
 
 import Data.Maybe (Maybe(..), maybe)
-import Effect.Uncurried (mkEffectFn1)
 import React.Basic (Component, JSX, createComponent, make)
 import React.Basic (fragment) as React
-import React.Basic.Native (string, text, textInput) as RN
+import React.Basic.Events (merge, EventFn)
+import React.Basic.Native (TextInputChangeEventData, string, text, textInput) as RN
+import React.Basic.Native.Events (NativeSyntheticEvent, capture, nativeEvent, timeStamp) as RNE
 
 app :: JSX
 app = controlledInput unit
@@ -25,15 +26,17 @@ controlledInput = make component
   , render: \self -> 
       React.fragment 
         [ RN.textInput {
-            key : "1",
-            onChange: mkEffectFn1 (\event -> do
-                event.persist
-                self.setState _ { value = event.nativeEvent.text, timestamp = Just event.timeStamp })
- 
+            key : "1"
+          , onChange: do
+              let (eventFn1 :: EventFn (RNE.NativeSyntheticEvent RN.TextInputChangeEventData) RN.TextInputChangeEventData) = RNE.nativeEvent
+              let (eventFn2 :: EventFn (RNE.NativeSyntheticEvent RN.TextInputChangeEventData) Number) = RNE.timeStamp
+              RNE.capture (merge { nativeEvent: eventFn1, timeStamp : eventFn2 }) \{ nativeEvent, timeStamp} -> 
+                self.setState _ { value = nativeEvent.text, timestamp = Just timeStamp }
           , value: self.state.value
         }
         , RN.text { key : "2", children : [ RN.string ("Current value = " <> show self.state.value) ] }
         , RN.text { key : "3", children : [ RN.string ("Changed at = " <> maybe "never" show self.state.timestamp) ] }
         ] 
   }
+
 
--- src/React/Basic/Native/Events.purs
+++ src/React/Basic/Native/Events.purs
@@ -3,7 +3,41 @@ module React.Basic.Native.Events where
 import Prelude
 
 import Effect (Effect)
+import Effect.Uncurried (EffectFn1, mkEffectFn1)
+import Effect.Unsafe (unsafePerformEffect)
+import React.Basic.Events (EventFn, unsafeEventFn)
+import Unsafe.Coerce (unsafeCoerce)
 
+foreign import data NativeSyntheticEvent :: Type -> Type
+
+type EventHandler e = EffectFn1 (NativeSyntheticEvent e) Unit
+
+handler :: forall e a. EventFn (NativeSyntheticEvent e) a -> (a -> Effect Unit) -> EventHandler e
+handler efn cb = mkEffectFn1 $ (unsafeCoerce efn) >>> cb
+
+capture :: forall e a. EventFn (NativeSyntheticEvent e) a -> (a -> Effect Unit) -> EventHandler e
+capture eventFn = handler (preventDefault >>> stopPropagation >>> eventFn)
+
+capture_ :: forall e. Effect Unit -> EventHandler e
+capture_ cb = capture identity \_ -> cb
+
+timeStamp :: forall e. EventFn (NativeSyntheticEvent e) Number
+timeStamp = unsafeEventFn \e -> (unsafeCoerce e).timeStamp
+
+nativeEvent :: forall e. EventFn (NativeSyntheticEvent e) e
+nativeEvent = unsafeEventFn \e -> (unsafeCoerce e).nativeEvent
+
+preventDefault :: forall e. EventFn (NativeSyntheticEvent e) (NativeSyntheticEvent e)
+preventDefault = unsafeEventFn \e -> unsafePerformEffect do
+  _ <- (unsafeCoerce e).preventDefault
+  pure e
+
+stopPropagation :: forall e. EventFn (NativeSyntheticEvent e) (NativeSyntheticEvent e)
+stopPropagation = unsafeEventFn \e -> unsafePerformEffect do
+  _ <- (unsafeCoerce e).stopPropagation
+  pure e
+
+{-
 type NativeSyntheticEvent e = {
   nativeEvent :: e 
 , currentTarget :: Number 
@@ -21,3 +55,4 @@ type NativeSyntheticEvent e = {
 , timeStamp :: Number
 , type :: String
 }
+-}
